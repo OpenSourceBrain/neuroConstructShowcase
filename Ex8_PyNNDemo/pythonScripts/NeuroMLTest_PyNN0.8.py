2991cd2324ff62384c79a8e52bd3a78f64807aa6
Padraig Gleeson <p.gleeson@gmail.com>
2016-01-27 19:16:51 +0000

    Updates to get more in line with standard pynn examples

--- Ex8_PyNNDemo/pythonScripts/NeuroMLTest_PyNN0.8.py
+++ Ex8_PyNNDemo/pythonScripts/NeuroMLTest_PyNN0.8.py
@@ -7,58 +7,59 @@ logging.basicConfig(format='%(levelname)s - %(name)s: %(message)s', level=loggin
 
 import sys
 import os
+from importlib import import_module
 
 from pyNN.utility import get_script_args
 
 simulator_name = get_script_args(1)[0]  
-exec("from pyNN.%s import *" % simulator_name)
+sim = import_module("pyNN.%s" % simulator_name)
 
 tstop = 500.0
 
-setup(timestep=0.01)
+sim.setup(timestep=0.01, debug=True)
     
 cell_params1 = {'tau_refrac':10,'v_thresh':-52.0, 'v_reset':-62.0, 'i_offset': 0.9, 'tau_syn_E'  : 2.0, 'tau_syn_I': 5.0}
-pop_IF_curr_alpha = Population(1, IF_curr_alpha, cell_params1, label="pop_IF_curr_alpha")
+pop_IF_curr_alpha = sim.Population(1, sim.IF_curr_alpha(**cell_params1), label="pop_IF_curr_alpha")
 pop_IF_curr_alpha.record('v')
 #pop_IF_curr_alpha.initialize('v', -67)
 
 cell_params2 = {'tau_refrac':8,'v_thresh':-50.0, 'v_reset':-70.0, 'i_offset': 1, 'tau_syn_E'  : 2.0, 'tau_syn_I': 5.0}
-pop_IF_curr_exp = Population(1, IF_curr_exp, cell_params2, label="pop_IF_curr_exp")
+pop_IF_curr_exp = sim.Population(1, sim.IF_curr_exp(**cell_params2), label="pop_IF_curr_exp")
 pop_IF_curr_exp.record('v')
 #pop_IF_curr_exp.initialize('v', -68)
 
 cell_params3 = {'tau_refrac':5,'v_thresh':-50.0, 'v_reset':-65.0, 'i_offset': 0.9, 'tau_syn_E'  : 2.0, 'tau_syn_I': 5.0}
-pop_IF_cond_alpha = Population(1, IF_cond_alpha, cell_params3, label="pop_IF_cond_alpha")
+pop_IF_cond_alpha = sim.Population(1, sim.IF_cond_alpha(**cell_params3), label="pop_IF_cond_alpha")
 pop_IF_cond_alpha.record('v')
 #pop_IF_cond_alpha.initialize('v', -69)
 
 cell_params4 = {'tau_refrac':5,'v_thresh':-52.0, 'v_reset':-68.0, 'i_offset': 1, 'tau_syn_E'  : 2.0, 'tau_syn_I': 5.0}
-pop_IF_cond_exp = Population(1, IF_cond_exp, cell_params4, label="pop_IF_cond_exp")
+pop_IF_cond_exp = sim.Population(1, sim.IF_cond_exp(**cell_params4), label="pop_IF_cond_exp")
 pop_IF_cond_exp.record('v')
 #pop_IF_cond_exp.initialize('v', -70)
 
 ##TODO: Test a>0!!
 cell_params5 = {'tau_refrac':0,'v_thresh':-52.0, 'v_reset':-68.0, 'i_offset': 0.6, 'v_spike': -40, 'a': 0.0, 'b':0.0805}
-pop_EIF_cond_exp_isfa_ista = Population(1, EIF_cond_exp_isfa_ista, cell_params5, label="pop_EIF_cond_exp_isfa_ista")
+pop_EIF_cond_exp_isfa_ista = sim.Population(1, sim.EIF_cond_exp_isfa_ista(**cell_params5), label="pop_EIF_cond_exp_isfa_ista")
 pop_EIF_cond_exp_isfa_ista.record('v')
 
 
 cell_params6 = {'i_offset': 0.2, 'gbar_K':6.0, 'gbar_Na':20.0}
-pop_HH_cond_exp = Population(1, HH_cond_exp, cell_params6, label="pop_HH_cond_exp")
+pop_HH_cond_exp = sim.Population(1, sim.HH_cond_exp(**cell_params6), label="pop_HH_cond_exp")
 pop_HH_cond_exp.record('v')
 
 # Post synaptic cells
 cell_params_post1 = {'tau_refrac':10,'v_thresh':-52.0, 'v_reset':-62.0, 'i_offset': 0}
-pop_post1 = Population(1, IF_cond_exp, cell_params_post1, label="pop_post1")
+pop_post1 = sim.Population(1, sim.IF_cond_exp(**cell_params_post1), label="pop_post1")
 pop_post1.record('v')
 cell_params_post2 = {'tau_refrac':10,'v_thresh':-52.0, 'v_reset':-62.0, 'i_offset': 0}
-pop_post2 = Population(1, IF_cond_exp, cell_params_post2, label="pop_post2")
+pop_post2 = sim.Population(1, sim.IF_cond_exp(**cell_params_post2), label="pop_post2")
 pop_post2.record('v')
 
-connE = connect(pop_EIF_cond_exp_isfa_ista, pop_post1, weight=0.006, receptor_type='excitatory',delay=5)
-connE = connect(pop_EIF_cond_exp_isfa_ista, pop_post2, weight=0.003, receptor_type='excitatory',delay=10)
+connE = sim.connect(pop_EIF_cond_exp_isfa_ista, pop_post1, weight=0.006, receptor_type='excitatory',delay=5)
+connE = sim.connect(pop_EIF_cond_exp_isfa_ista, pop_post2, weight=0.003, receptor_type='excitatory',delay=10)
 
-run(tstop)
+sim.run(tstop)
 
 from neo.io import NeoHdf5IO
 results_file = "Results/NeuroMLTest_%s.h5" % simulator_name
@@ -77,7 +78,7 @@ pop_post1.write_data(io)
 pop_post2.write_data(io)
 
 
-end()
+sim.end()
 
 if not '-nogui' in sys.argv:
     if simulator_name in ['neuron', 'nest', 'brian']:
